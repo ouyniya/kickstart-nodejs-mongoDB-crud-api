1716429cf0ade602b2f59730ee0773e370ada637
niya-s <niya.somkerd@gmail.com>
2025-04-29 14:51:54 +0700

    feat: crud api

--- controllers/product-controller.js
+++ controllers/product-controller.js
@@ -1,36 +1,62 @@
-exports.read = async(req, res) => {
-    res.send('hello reading')
-}
+const Product = require("../models/product");
 
-exports.list = async(req, res) => {
-    try {
-        // console.log(g)
-        res.send('hello list')
-    } catch (error) {
-        res.send('server error')
-    }
-}
+exports.read = async (req, res) => {
+  try {
+    const { id } = req.params;
+    const productSent = await Product.findOne({
+      _id: id,
+    }).exec();
+    res.send(productSent);
+  } catch (error) {
+    console.log(error);
+  }
+};
 
-exports.create = async(req, res) => {
-    try {
-        res.send('hello create')
-    } catch (error) {
-        res.send('server error')
-    }
-}
+exports.list = async (req, res) => {
+  try {
+    const productSent = await Product.find({}).exec();
 
-exports.update = async(req, res) => {
-    try {
-        res.send('hello update')
-    } catch (error) {
-        res.send('server error')
-    }
-}
+    res.send(productSent);
+  } catch (error) {
+    res.send("server error");
+  }
+};
 
-exports.remove = async(req, res) => {
-    try {
-        res.send('hello delete')
-    } catch (error) {
-        res.send('server error')
-    }
-}
+exports.create = async (req, res) => {
+  try {
+    // const { name, detail, price } = req.body
+    const productSent = await Product(req.body).save();
+
+    res.send(productSent);
+  } catch (error) {
+    res.send("server error");
+  }
+};
+
+exports.update = async (req, res) => {
+  try {
+    const { id } = req.params;
+    const productSent = await Product.findOneAndUpdate(
+      {
+        _id: id,
+      },
+      req.body,
+      { new: true }
+    ).exec();
+    res.send(productSent);
+  } catch (error) {
+    res.send("server error");
+  }
+};
+
+exports.remove = async (req, res) => {
+  try {
+    const {id} = req.params
+    const removeProduct = await Product.findOneAndDelete({
+        _id: id
+    }).exec()
+    res.send(removeProduct);
+  } catch (error) {
+    res.send("server error");
+  }
+};
--- routes/product-route.js
+++ routes/product-route.js
@@ -6,7 +6,7 @@ router.get('/product', list)
 
 router.get('/product/:id', read)
 
-router.post('/product/:id', create)
+router.post('/product', create)
 
 router.put('/product/:id', update)
 
